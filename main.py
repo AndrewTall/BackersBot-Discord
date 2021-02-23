e0825be8ef21bc4d9d8fdfc7f8e64cba337dcf0b
Andrey Dolgy <andrewtall93@gmail.com>
2021-02-23 11:47:06 +0700

    prefix format; cog

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ client = commands.Bot(command_prefix=prefix)
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game('Listening to {}help'.format(prefix)))
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('Listening to {0}help'.format(prefix)))
     print('I am online')
 
 
@@ -62,158 +62,171 @@ async def on_command_error(ctx: commands.Context, error: str):
 
 
 # region Backer Roles
-@client.command(brief='Backer verification help')
-async def backer_help(ctx: commands.Context):
-    log_command(ctx.message.author, 'backer_help')
+class BackerVerification(commands.Cog, name='Backer verification'):
+    @client.command(brief='Backer verification help')
+    async def backer_help(self, ctx: commands.Context):
+        log_command(ctx.message.author, 'backer_help')
 
-    msg = 'This bot will help you identify yourself as a backer and obtain access to Discord\'s private channels.\r\r' \
-          'In order to start the process, you\'ll need to know the email you\'ve used to back our project. That would be ' \
-          'your Kickstarter email, PayPal email or your Facebook email if you have your Kickstarter and Facebook ' \
-          'accounts linked.\r\r' \
-          'Send me the following command: \r\r' \
-          '{0}backer_mail email@example.com'.format(prefix)
-    if ctx.message.channel.is_private:
-        await ctx.send(msg)
-    else:
-        await ctx.message.delete()
-        try:
-            await ctx.message.author.send(msg)
-        except discord.errors.Forbidden:
-            await ctx.message.channel.send(ctx.message.channel, '{0} you have disabled direct messages '
-                                                        'from this server members. '
-                                                        'Please, allow them temporarily so we can start the process.'
-                                   .format(ctx.message.author.mention))
+        msg = 'This bot will help you identify yourself as a backer and obtain access to Discord\'s private channels.\r\r' \
+            'In order to start the process, you\'ll need to know the email you\'ve used to back our project. That would be ' \
+            'your Kickstarter email, PayPal email or your Facebook email if you have your Kickstarter and Facebook ' \
+            'accounts linked.\r\r' \
+            'Send me the following command: \r\r' \
+            '{0}backer_mail email@example.com'.format(prefix)
+        if ctx.message.channel.is_private:
+            await ctx.send(msg)
+        else:
+            await ctx.message.delete()
+            try:
+                await ctx.message.author.send(msg)
+            except discord.errors.Forbidden:
+                await ctx.reply(ctx.message.channel, '{0} you have disabled direct messages '
+                                                            'from this server members. '
+                                                            'Please, allow them temporarily so we can start the process.'
+                                    .format(ctx.message.author.mention))
 
 
-@client.command(brief='Initiate backer\'s email verification')
-async def backer_mail(ctx: commands.Context, email: str):
-    log_command(ctx.message.author, 'backer_mail', email)
+    @client.command(brief='Initiate backer\'s email verification')
+    async def backer_mail(self, ctx: commands.Context, email: str):
+        log_command(ctx.message.author, 'backer_mail', email)
 
-    # Only works if we're on a private message
-    if ctx.message.channel.is_private:
-        # Check if email is valid
-        if valid_email(email):
-            # Check the Database and see if we have the email.
-            # Also check it we already sent a verification code and send the same one
+        # Only works if we're on a private message
+        if ctx.message.channel.is_private:
+            # Check if email is valid
+            if valid_email(email):
+                # Check the Database and see if we have the email.
+                # Also check it we already sent a verification code and send the same one
+                db = db_connect()
+                try:
+                    with db.cursor() as cursor:
+                        cursor.execute('SELECT `verification_code` FROM `backers` WHERE `email`=%s', email)
+                        result = cursor.fetchone()
+
+                        token = None
+
+                        if result is None:
+                            # User doesn't exists in the database. Throw an error.
+                            await ctx.send('The email address is not registered as a valid backer. '
+                                        'Please, make sure you\'ve entered the right email.\r\r')
+                        elif result['verification_code'] is None:
+                            # User hasn't started the verified proccess previously. Generate a new verifiy token.
+                            token = generate_random_string(40)
+
+                            # Save the token on the database.
+                            cursor.execute('UPDATE `backers` SET `verification_code`=%s'
+                                        ' WHERE `email`=%s', (token, email))
+                            db.commit()
+                        else:
+                            # Get previous token and reuse it.
+                            # token = result['verification_code']
+                            ctx.send('We\'ve already send you verification email, please check your inbox and spam folder.')
+
+                        if token is not None:
+                            # Send an email with the token and say the instructions to verify it.
+                            requests.post('https://api.mailgun.net/v2/{0}/messages'.format(mailgun_host),
+                                        auth=('api', mailgun_key),
+                                        data={
+                                            'from': '{0}'.format(mailgun_email),
+                                            'to': email,
+                                            'subject': 'Discord: Email Verification',
+                                            'html': 'Hello Backer! <br/><br/>'
+                                                    'This is a confirmation email to verify you as one of our '
+                                                    'backers. In order to confirm you as a backer, please go to Discord '
+                                                    'and send the following message to BackersBot: <br/><br/>'
+                                                    '{0}backer_verify {1} {2}'.format(prefix, email, token)
+                                        })
+
+                            await ctx.send('Welcome backer! Just one more step to access the backer-exclusive channels. '
+                                        'Please, check your email for the verification code we just sent you (please '
+                                        'check your spam folder too just in case) and send '
+                                        'me back the following command:\r\r'
+                                        '{0}backer_verify {1} verification_code_here'
+                                        .format(prefix, email))
+                finally:
+                    cursor.close()
+                    db.close()
+            else:
+                await ctx.send('The email address looks like it\'s invalid. '
+                            'Please, make sure you enter a valid email address.')
+        else:
+            await ctx.message.delete()
+            try:
+                await ctx.message.author.send('That command only works on private message. '
+                                            'Please send me the command again.')
+            except discord.errors.Forbidden:
+                await ctx.reply(ctx.message.channel, '{0} you have disabled direct messages '
+                                'from this server members. '
+                                'Please, allow them temporarily so we can start the process.'
+                                .format(ctx.message.author.mention))
+
+
+    @client.command(brief='Verify backer\'s email')
+    async def backer_verify(self, ctx: commands.Context, email: str, token: str):
+        log_command(ctx.message.author, 'backer_verify', email, token)
+
+        # Only works if we're on a private message
+        if ctx.message.channel.is_private:
+            # Connect to the database and check if the email-token is correct
             db = db_connect()
             try:
                 with db.cursor() as cursor:
-                    cursor.execute('SELECT `verification_code` FROM `backers` WHERE `email`=%s', email)
+                    cursor.execute('SELECT `discord_user_id`, `role_id` FROM `backers` WHERE `email`=%s'
+                                ' AND `verification_code`=%s',
+                                (email, token))
                     result = cursor.fetchone()
-
-                    token = None
 
                     if result is None:
                         # User doesn't exists in the database. Throw an error.
-                        await ctx.send('The email address is not registered as a valid backer. '
-                                      'Please, make sure you\'ve entered the right email.\r\r')
-                    elif result['verification_code'] is None:
-                        # User hasn't started the verified proccess previously. Generate a new verifiy token.
-                        token = generate_random_string(40)
+                        await ctx.send('The combination of user and verification code doesn\'t exist. '
+                                    'Please, make sure you\'ve entered the right email and code.\r\r')
+                    elif result['discord_user_id'] == ctx.message.author.id:
+                        # The user is already registered
+                        server = client.get_guild(id=server_id)
+                        server_member = discord.utils.get(server.members, id=ctx.message.author.id)
+                        if server_member is not None:
+                            server_role = discord.utils.get(server.roles, id=result['role_id'])
+                            await server_member.add_roles(server_role)
 
-                        # Save the token on the database.
-                        cursor.execute('UPDATE `backers` SET `verification_code`=%s'
-                                       ' WHERE `email`=%s', (token, email))
-                        db.commit()
+                        await ctx.send('You\'ve already been confirmed as a backer.')
+                    elif result['discord_user_id'] is not None:
+                        # Someone already registered this email.
+                        await ctx.send('It looks like this email has already been registered by another user.')
                     else:
-                        # Get previous token and reuse it.
-                        # token = result['verification_code']
-                        ctx.send('We\'ve already send you verification email, please check your inbox and spam folder.')
+                        # Check if the user has joined server
+                        server = client.get_guild(id=server_id)
+                        server_member = discord.utils.get(server.members, id=ctx.message.author.id)
+                        if server_member is not None:
+                            # Update the database to register this user as taken
+                            cursor.execute('UPDATE `backers` SET `discord_user_id`=%s'
+                                        ' WHERE `email`=%s AND `verification_code`=%s',
+                                        (ctx.message.author.id, email, token))
+                            db.commit()
 
-                    if token is not None:
-                        # Send an email with the token and say the instructions to verify it.
-                        requests.post('https://api.mailgun.net/v2/{0}/messages'.format(mailgun_host),
-                                      auth=('api', mailgun_key),
-                                      data={
-                                          'from': '{0}'.format(mailgun_email),
-                                          'to': email,
-                                          'subject': 'Discord: Email Verification',
-                                          'html': 'Hello Backer! <br/><br/>'
-                                                  'This is a confirmation email to verify you as one of our '
-                                                  'backers. In order to confirm you as a backer, please go to Discord '
-                                                  'and send the following message to BackersBot: <br/><br/>'
-                                                  '{0}backer_verify {1} {2}'.format(prefix, email, token)
-                                      })
+                            server_role = discord.utils.get(server.roles, id=result['role_id'])
 
-                        await ctx.send('Welcome backer! Just one more step to access the backer-exclusive channels. '
-                                      'Please, check your email for the verification code we just sent you (please '
-                                      'check your spam folder too just in case) and send '
-                                      'me back the following command:\r\r'
-                                      '{0}backer_verify {1} verification_code_here'
-                                      .format(prefix, email))
+                            await server_member.add_roles(server_role)
+                            await ctx.send(
+                                'Congratulations! You just completed the process and you\'ve been confirmed as '
+                                'a **{0}** tier backer.'
+                                .format(server_role.name))
+                        elif server_invite_link:
+                            await ctx.send(
+                                'You haven\'t joined our Discord server! You should join it first and then come '
+                                'back and run the command again.\r\r'
+                                'Please, join the server here: {0}'.format(server_invite_link))
             finally:
                 cursor.close()
                 db.close()
         else:
-            await ctx.send('The email address looks like it\'s invalid. '
-                          'Please, make sure you enter a valid email address.')
-    else:
-        await ctx.message.delete()
-        await ctx.message.author.send('That command only works on private message. '
-                                                   'Please send me the command again.')
-
-
-@client.command(brief='Verify backer\'s email')
-async def backer_verify(ctx: commands.Context, email: str, token: str):
-    log_command(ctx.message.author, 'backer_verify', email, token)
-
-    # Only works if we're on a private message
-    if ctx.message.channel.is_private:
-        # Connect to the database and check if the email-token is correct
-        db = db_connect()
-        try:
-            with db.cursor() as cursor:
-                cursor.execute('SELECT `discord_user_id`, `role_id` FROM `backers` WHERE `email`=%s'
-                               ' AND `verification_code`=%s',
-                               (email, token))
-                result = cursor.fetchone()
-
-                if result is None:
-                    # User doesn't exists in the database. Throw an error.
-                    await ctx.send('The combination of user and verification code doesn\'t exist. '
-                                  'Please, make sure you\'ve entered the right email and code.\r\r')
-                elif result['discord_user_id'] == ctx.message.author.id:
-                    # The user is already registered
-                    server = client.get_guild(id=server_id)
-                    server_member = discord.utils.get(server.members, id=ctx.message.author.id)
-                    if server_member is not None:
-                        server_role = discord.utils.get(server.roles, id=result['role_id'])
-                        await server_member.add_roles(server_role)
-
-                    await ctx.send('You\'ve already been confirmed as a backer.')
-                elif result['discord_user_id'] is not None:
-                    # Someone already registered this email.
-                    await ctx.send('It looks like this email has already been registered by another user.')
-                else:
-                    # Check if the user has joined server
-                    server = client.get_guild(id=server_id)
-                    server_member = discord.utils.get(server.members, id=ctx.message.author.id)
-                    if server_member is not None:
-                        # Update the database to register this user as taken
-                        cursor.execute('UPDATE `backers` SET `discord_user_id`=%s'
-                                       ' WHERE `email`=%s AND `verification_code`=%s',
-                                       (ctx.message.author.id, email, token))
-                        db.commit()
-
-                        server_role = discord.utils.get(server.roles, id=result['role_id'])
-
-                        await server_member.add_roles(server_role)
-                        await ctx.send(
-                            'Congratulations! You just completed the process and you\'ve been confirmed as '
-                            'a **{0}** tier backer.'
-                            .format(server_role.name))
-                    elif server_invite_link:
-                        await ctx.send(
-                            'You haven\'t joined our Discord server! You should join it first and then come '
-                            'back and run the command again.\r\r'
-                            'Please, join the server here: {0}'.format(server_invite_link))
-        finally:
-            cursor.close()
-            db.close()
-    else:
-        await ctx.message.delete()
-        await ctx.message.author.send('That command only works on private message. '
-                                                   'Please send me the command again.')
+            await ctx.message.delete()
+            try:
+                await ctx.message.author.send('That command only works on private message. '
+                                            'Please send me the command again.')
+            except discord.errors.Forbidden:
+                await ctx.reply(ctx.message.channel, '{0} you have disabled direct messages '
+                                'from this server members. '
+                                'Please, allow them temporarily so we can start the process.'
+                                .format(ctx.message.author.mention))
 # endregion
 
 
@@ -243,4 +256,5 @@ def generate_random_string(size = 20, chars = string.ascii_uppercase + string.di
 # endregion
 
 
+client.add_cog(BackerVerification(client))
 client.run(bot_token)
