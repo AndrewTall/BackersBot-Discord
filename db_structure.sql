2ee93ce9a71ccc177e0c48a19c18ee017349c530
Andrey Dolgy <andrewtall93@gmail.com>
2021-02-23 14:56:11 +0700

    fix sql scheme again

--- db_structure.sql
+++ db_structure.sql
@@ -6,7 +6,7 @@ CREATE TABLE backers (
   "email" varchar(255) NOT NULL,
   "role_id" bigint NOT NULL,
   "verification_code" varchar(40) DEFAULT NULL,
-  "discord_user_id" varchar(40) DEFAULT NULL,
+  "discord_user_id" bigint DEFAULT NULL,
   CONSTRAINT "PK_backers_email" PRIMARY KEY ("email"),
   CONSTRAINT "UK_backers_email" UNIQUE ("email")
 );
