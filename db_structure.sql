cdb22c8a2665cf1915d01b36ea3b152f54f4da60
Andrey Dolgy <andrewtall93@gmail.com>
2021-02-23 12:34:28 +0700

    update sql

--- db_structure.sql
+++ db_structure.sql
@@ -1,19 +1,14 @@
 --
--- Table structure for table `backers`
+-- Table structure for table "backers"
 --
 
-CREATE TABLE `backers` (
-  `email` varchar(255) NOT NULL,
-  `role_id` varchar(40) NOT NULL,
-  `verification_code` varchar(40) DEFAULT NULL,
-  `discord_user_id` varchar(40) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE backers (
+  "email" varchar(255) NOT NULL,
+  "role_id" varchar(40) NOT NULL,
+  "verification_code" varchar(40) DEFAULT NULL,
+  "discord_user_id" varchar(40) DEFAULT NULL,
+  CONSTRAINT "PK_backers_email" PRIMARY KEY ("email"),
+  CONSTRAINT "UK_backers_email" UNIQUE ("email")
+);
 
---
--- Indexes for table `backers`
---
-ALTER TABLE `backers`
-  ADD PRIMARY KEY (`email`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `verification_code` (`verification_code`);
-COMMIT;
+CREATE INDEX I_backers_email ON backers ("verification_code");
